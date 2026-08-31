67e2c35fb1a3ba6ed806882abbd525b259a4d395
Norb <norblara@gmail.com>
2026-08-31 18:32:46 -0500

    test: add failing test for Profile model creation

--- social/tests.py
+++ social/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from.models import Profile
 
-# Create your tests here.
+class ProfileModelTest(TestCase):
+    def test_profile_can_be_created(self):
+        Profile.objects.create()
