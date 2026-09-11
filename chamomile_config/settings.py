7a50387140fb7150eb590f389258ac6edbc9bf9f
Norb <norblara@gmail.com>
2026-09-11 10:23:56 -0500

    chore: add GitHub Actions CI sorkflow and move SECRET_KEY TO env

--- chamomile_config/settings.py
+++ chamomile_config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*i2n9+rd#cj9h)x&&55ukp-h%o!$v4*-vw#s2s02eu2z*r1dj1'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
