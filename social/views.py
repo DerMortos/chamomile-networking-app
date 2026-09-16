978df693214a1db0b6ad0771368df14e9b31c1c1
Norb <norblara@gmail.com>
2026-09-16 10:43:39 -0500

    feat: add stub views for all routes

--- social/views.py
+++ social/views.py
@@ -1,1 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+class SignUpView(View):
+    def get(self, request):
+        return HttpResponse("signup stub")
+def feed(request):
+    return HttpResponse("feed stub")
+
+def profile(request, username):
+    return HttpResponse("profile stub")
+
+def toggle_follow(request, username):
+    return HttpResponse("follow stub")
+
+def member_list(request):
+    return HttpResponse("members stub")
+
+def post_detail(request,  pk):
+    return HttpResponse("post stub")
+
+def edit_profile(request):
+    return HttpResponse("edit profile stub")
+
+def delete_message(request, pk):
+    return HttpResponse("delete message stub")
